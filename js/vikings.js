ada150943c968767c47752b96471e8469bea427f
Javier <javipascualgonzalez@gmail.com>
2017-12-04 14:43:50 +0100

    Correcion con objetos

--- js/vikings.js
+++ js/vikings.js
@@ -1,3 +1,45 @@
+
+function Viking(name,health,strength)
+{
+	this.name = name;
+	this.health = health;
+	this.strength = strength;
+}
+
+function Pit(viking1, viking2, turns) {
+	this.viking1 = viking1;
+	this.viking2 = viking2;
+	this.turns = turns;
+}
+
+Viking.prototype.hit = function(enemy){
+	var hitDamage = this.strength;
+	enemy.health -= hitDamage;
+}
+
+Pit.prototype.fight = function(){
+	var minHealth = 7;
+	while (this.turns > 0 && this.viking1.health > minHealth && this.viking2.health > minHealth) {
+		this.viking1.hit(this.viking2);
+		this.viking2.hit(this.viking1);
+		this.turns--;
+	}
+	this.winner = this.viking1.health > this.viking2.health ? this.viking1 : this.viking2;
+}
+Pit.prototype.showWinner = function() {
+	console.log("And the winner is " + this.winner.name);
+}
+
+var v1 = new Viking("Juan",90,10);
+var v2 = new Viking("Pedro",90,10);
+var pit = new Pit(v1,v2,8)
+
+
+
+ 
+
+
+/*
 var indexJugador1;
 var indexJugador2;
 var limit = 10;
@@ -25,6 +67,7 @@ var pedro = new Viking("Pedro",50,5);
 var raul = new Viking("Raul",50,5);
 var ruben = new Viking("Ruben",50,5);
 var javi = new Viking("Javi",50,5);
+
 var vikings = [juan,pedro,raul,ruben,javi];
 var saxons = [saxon]
 
@@ -57,5 +100,4 @@ function randomIntFromInterval(min,max)
 {
     return Math.floor(Math.random()*(max-min+1)+min);
 }
-
-
+*/
